6c78b91ee5d9ffccf58d6a046bd1ff5c275422b1
Zena Brock <zenabrock@gmail.com>
2025-03-19 13:28:54 -0400

    feat: Add novel-next.com
    Improved-by: Clocks <doomsdayrs@gmail.com>

---
+++ src/en/Novel-next.lua
@@ -0,0 +1,14 @@
+-- {"id":17692548,"ver":"1.0.0","libVer":"1.0.0","author":"OceaniaRose":,"dep"["NovelFull>=2.0.2"]}
+
+return Require("NovelFull")("http://novel-next.com", {
+	id = 17692548,
+	name = "Novel-Next",
+	imageURL = "https://novel-next.com/img/logo.png",
+	
+	meta_offset = 0,
+	ajax_hot = "/ajax-search?type=hot",
+	ajax_latest = "/ajax-search?type=latest",
+	ajax_chapters = "/ajax-chapter-option",
+	searchListSel = "list.list-truyen.col-xs-12",
+	searchTitleSel = ".truyen-title"
+})
