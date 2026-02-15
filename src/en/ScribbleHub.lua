66dca71aaa2c7a12700519a48567d55872c597e1
Epic Rpg <forepicrpg7@gmail.com>
2026-02-15 01:47:29 +0000

    Update 2 files
    - /src/en/ScribbleHub.lua
    - /index.json

--- src/en/ScribbleHub.lua
+++ src/en/ScribbleHub.lua
@@ -1,4 +1,4 @@
--- {"id":86802,"ver":"1.2.04","libVer":"1.0.0","author":"TechnoJo4, StormX4","dep":["url>=1.0.0","CommonCSS>=1.0.0","unhtml>=1.0.0"]}
+-- {"id":86802,"ver":"1.2.4","libVer":"1.0.0","author":"TechnoJo4, StormX4","dep":["url>=1.0.0","CommonCSS>=1.0.0","unhtml>=1.0.0"]}
 
 local baseURL = "https://www.scribblehub.com"
 
