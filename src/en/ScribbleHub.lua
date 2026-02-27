506e617f24d8f8d8c1682f0442e5a3d7004a0a51
Levv616 <itsecretlev4@gmail.com>
2026-02-27 08:19:52 +0800

    [skip ci]

--- src/en/ScribbleHub.lua
+++ src/en/ScribbleHub.lua
@@ -1,4 +1,4 @@
--- {"id":86802,"ver":"1.2.9","libVer":"1.0.0","author":"TechnoJo4, StormX4 (updated by lev616)","dep":["url>=1.0.0","CommonCSS>=1.0.0","unhtml>=1.0.0"]}
+-- {"id":86802,"ver":"1.3.1","libVer":"1.0.0","author":"TechnoJo4, StormX4 (updated by lev616)","dep":["url>=1.0.0","CommonCSS>=1.0.0","unhtml>=1.0.0"]}
 
 local baseURL = "https://www.scribblehub.com"
 local qs = Require("url").querystring
