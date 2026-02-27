da84cfd0b51a8534a093de16a1b201208c847142
Levv616 <itsecretlev4@gmail.com>
2026-02-27 09:17:02 +0800

    [skip ci]

--- src/en/DobyTranslations.lua
+++ src/en/DobyTranslations.lua
@@ -19,8 +19,8 @@ return Require("Madara")("https://dobytranslations.com", {
     hasCloudFlare = false,
     isSearchIncrementing = true,
 
-    latest = function(self, data)
-        local page = data[PAGE]
+    latest = function(self, page)
+        page = page or 1
         local url = self.baseURL .. "/series/?page=" .. page .. "&m_orderby=latest"
         return self:parse(GETDocument(url))
     end,
