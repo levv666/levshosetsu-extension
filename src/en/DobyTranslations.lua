52957ed72e22669da1957ad077f114a0fa34aeab
Levv616 <itsecretlev4@gmail.com>
2026-02-27 11:41:28 +0800

    [skip ci]

--- src/en/DobyTranslations.lua
+++ src/en/DobyTranslations.lua
@@ -83,7 +83,7 @@ local function parseNovel(novelURL, loadChapters)
         title = titleElement and titleElement:text() or "No Title",
         imageURL = imageElement and imageElement:attr("src") or nil,
         description = descriptionElement and descriptionElement:text() or "",
-        genres = genrelist and map(genrelist:select("a[rel=tag]"), text),
+        genres = genrelist and map(genrelist:select("a[rel=tag]"), function(v) return v:text() end) or nil,
         status = NovelStatus.UNKNOWN
     }
 
