c5b0eb95a0d2ae9252ea5d3ebbb3b2696a069775
Levv616 <itsecretlev4@gmail.com>
2026-02-27 09:09:28 +0800

    [skip ci]

--- src/en/DobyTranslations.lua
+++ src/en/DobyTranslations.lua
@@ -24,7 +24,7 @@ return Require("Madara")("https://dobytranslations.com", {
     latest = function(self, data)
         local page = data[PAGE]
         local url = self.baseURL .. "/series/?page=" .. page .. "&m_orderby=latest"
-        return self.parse(GETDocument(url))
+        return self:parse(GETDocument(url))
     end,
 
     genres = {
