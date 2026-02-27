60e9a7badae64e48f9f5e954464e3c9cc73ed470
Levv616 <itsecretlev4@gmail.com>
2026-02-27 09:05:13 +0800

    [skip ci]

---
+++ src/en/DobyTranslations.lua
@@ -0,0 +1,49 @@
+-- {"id":8437659,"ver":"1.0.0","libVer":"1.0.0","author":"lev616"}
+
+-- {"id": 91919124, "ver": "1.0.0", "libVer": "2.9.2", "author": "muzdalifah", "dep": ["Madara>=2.2.0"]}
+
+return Require("Madara")("https://dobytranslations.com", {
+    id = 91919124, -- ⚠️ change to unique ID
+    name = "Doby Translations",
+    imageURL = "https://dobytranslations.com/wp-content/uploads/2024/05/a-adult-male-maltes-white-dog-reading-a-book.jpg",
+
+    novelListingURLPath = "series",
+    shrinkURLNovel = "series",
+
+    latestNovelSel = "a.series-link",
+    searchNovelSel = "a.series-link",
+
+    novelPageTitleSel = "h1",
+
+    chaptersScriptLoaded = true,
+    chaptersOrderReversed = true,
+
+    hasCloudFlare = false,
+    isSearchIncrementing = true,
+
+    genres = {
+        "Antihero Protagonist",
+        "Comedy",
+        "Completed Premium/Ongoing Free Unlock",
+        "Completely Free",
+        "Cthulhu",
+        "Drama",
+        "Ecchi",
+        "Fantasy",
+        "Farming",
+        "Female Protagonist",
+        "Gender Bender",
+        "Harem",
+        "Horror",
+        "Male Protagonist",
+        "Male to Female",
+        "No Romance",
+        "Psychological",
+        "Revenge",
+        "Romance",
+        "Slice of Life",
+        "System",
+        "Villain Protagonist",
+        "Yuri"
+    }
+})
