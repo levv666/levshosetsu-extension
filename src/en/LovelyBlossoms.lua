b584b51ea4a12d97a1139f17a3f589b3406ab040
Levv616 <itsecretlev4@gmail.com>
2026-03-01 20:33:47 +0800

    [skip ci]

--- src/en/LovelyBlossoms.lua
+++ src/en/LovelyBlossoms.lua
@@ -1,14 +1,17 @@
--- {"id":1742321,"ver":"1.0.3","libVer":"1.0.0","author":"Lev616","dep":["Madara>=2.2.0"]}
+-- {"id":1742321,"ver":"1.0.0","libVer":"1.0.0","author":"Lev616","dep":["Madara>=2.2.0"]}
 
 return Require("Madara")("https://lovelyblossoms.com", {
     id = 1742321,
     name = "Lovely Blossoms",
     imageURL = "https://lovelyblossoms.com/wp-content/uploads/2025/09/lovely-blossoms-2-Photoroom-1.png",
-
     chaptersScriptLoaded = true,
     novelPageTitleSel = "div.post-title > h1",
+
+    latestNovelSel = "div.page-listing-item",
+    novelListingURLPath = "novel",
+    shrinkURLNovel = "novel",
     searchHasOper = true,
-    chaptersListSelector = "li.wp-manga-chapter.free-chap",
+    chaptersListSelector= "li.wp-manga-chapter.free-chap",
 
     genres = {
         "Action",
@@ -43,3 +46,4 @@ return Require("Madara")("https://lovelyblossoms.com", {
         "Yuri"
     }
 })
+
