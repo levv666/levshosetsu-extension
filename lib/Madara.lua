4dd94dc403332b438e080b41ef1e14f94ee9b5a5
Levv616 <itsecretlev4@gmail.com>
2026-03-01 21:53:58 +0800

    [skip ci]

--- lib/Madara.lua
+++ lib/Madara.lua
@@ -90,9 +90,11 @@ function defaults:createSearchString(tbl)
 	if tbl[STATUS_FILTER_KEY_ON_HOLD] then
 		url = url .. "&status[]=on-hold"
 	end
+	local genreIndex = 0
 	for key, value in pairs(self.genres_map) do
 		if tbl[key] then
-			url = url .. "&genre[]=" .. value
+			url = url .. "&genre[" .. genreIndex .. "]=" .. value
+			genreIndex = genreIndex + 1
 		end
 	end
 
@@ -337,7 +339,7 @@ return function(baseURL, _self)
 		}),
 		FilterGroup("Genres", map(_self.genres, function(v, k)
 			keyID = keyID + 1
-			_self.genres_map[keyID] = v:lower():gsub(" ", "-")
+			_self.genres_map[keyID] = k or v:lower():gsub(" ", "-")
 			return CheckboxFilter(keyID, v)
 		end)) -- 6
 	}
