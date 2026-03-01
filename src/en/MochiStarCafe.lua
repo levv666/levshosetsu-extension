ae86dbbc73efff507c8c80e831592034431da509
Levv616 <itsecretlev4@gmail.com>
2026-03-02 01:24:00 +0800

    [skip ci]

--- src/en/MochiStarCafe.lua
+++ src/en/MochiStarCafe.lua
@@ -1,4 +1,4 @@
--- {"id":134652,"ver":"1.0.8","libVer":"1.0.0","author":"Lev616"}
+-- {"id":134652,"ver":"1.0.1","libVer":"1.0.0","author":"Lev616"}
 
 local baseURL = "https://mochistar.org"
 local DobyTranslationsLogo = "https://mochistar.org/wp-content/uploads/2026/02/stardust_mochi_129x129.png"
@@ -149,7 +149,14 @@ local function getPassage(chapterURL)
     local title = htmlElement:selectFirst("h1.chapter__title"):text()
     htmlElement = htmlElement:selectFirst("div.chapter-formatting")
     htmlElement:select("#wrap-button-remove-blur"):remove()
-    --- htmlElement:selectFirst("div.code-block"):remove() (for ads)
+    -- htmlElement:selectFirst("div.code-block"):remove() (for ads)
+    -- Remove empty paragraphs
+    --    for _, p in ipairs(htmlElement:select("p")) do
+    --        local text = p:text():gsub("%s+", "")
+    --        if text == "" then
+    --            p:remove()
+    --        end
+    --    end
     htmlElement:child(0):before("<h1>" .. title .. "</h1>");
     return HTMLToString(pageOfElem(htmlElement, true))
 end
