7c7d2ae90a229706e1bc68912bf5f86646904a8b
Levv616 <itsecretlev4@gmail.com>
2026-03-01 20:48:30 +0800

    [skip ci]

--- src/en/LovelyBlossoms.lua
+++ src/en/LovelyBlossoms.lua
@@ -1,17 +1,51 @@
--- {"id":1742321,"ver":"1.0.2","libVer":"1.0.0","author":"Lev616","dep":["Madara>=2.2.0"]}
+-- {"id":1742321,"ver":"1.0.3","libVer":"1.0.0","author":"Lev616","dep":["Madara>=2.2.0"]}
 
 return Require("Madara")("https://lovelyblossoms.com", {
     id = 1742321,
     name = "Lovely Blossoms",
     imageURL = "https://lovelyblossoms.com/wp-content/uploads/2025/09/lovely-blossoms-2-Photoroom-1.png",
+
     chaptersScriptLoaded = true,
     novelPageTitleSel = "div.post-title > h1",
 
     latestNovelSel = "div.page-listing-item",
     novelListingURLPath = "novel",
     shrinkURLNovel = "novel",
+
     searchHasOper = true,
-    chaptersListSelector= "li.wp-manga-chapter.free-chap",
+    chaptersListSelector = "li.wp-manga-chapter.free-chap",
+
+    -------------------------------------------------
+    -- 🔥 CUSTOM SEARCH BUILDER (no genres_map usage)
+    -------------------------------------------------
+    createSearchString = function(self, tbl)
+        local encode = Require("url").encode
+        local query = tbl[QUERY] or ""
+        local page = tbl[PAGE] or 1
+
+        local url = self.baseURL ..
+                "/page/" .. page .. "/?s=" .. encode(query) ..
+                "&post_type=wp-manga" ..
+                "&author=&artist=&release="
+
+        -- iterate through genres manually
+        local genreStartIndex = 100 -- Madara starts genre keys at 100
+        for i, name in ipairs(self.genres) do
+            local key = genreStartIndex + i
+            if tbl[key] then
+                local slug = name:lower():gsub(" ", "-")
+                url = url .. "&genre[]=" .. slug
+            end
+        end
+
+        -- operator
+        if self.searchOperId then
+            url = url .. "&op=" .. (tbl[self.searchOperId] and "0" or "1")
+        end
+
+        return url
+    end,
+    -------------------------------------------------
 
     genres = {
         "Action",
@@ -44,14 +78,5 @@ return Require("Madara")("https://lovelyblossoms.com", {
         "Urban",
         "Yaoi",
         "Yuri"
-    },
-
-    appendToSearchFilters = function(self, filters)
-        -- manually fix slugs
-        for key, value in pairs(self.genres_map) do
-            self.genres_map[key] = value:lower():gsub(" ", "-")
-        end
-        return filters
-    end,
+    }
 })
-
