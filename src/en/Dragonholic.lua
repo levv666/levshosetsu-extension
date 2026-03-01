e5b756da17053f67b4fe798e2f574c49cbc2fef4
Levv616 <itsecretlev4@gmail.com>
2026-03-01 23:19:02 +0800

    [skip ci]

--- src/en/Dragonholic.lua
+++ src/en/Dragonholic.lua
@@ -1,4 +1,4 @@
--- {"id":1567186593,"ver":"1.0.4","libVer":"1.0.0","author":"","repo":"","dep":[]}
+-- {"id":1567186593,"ver":"1.0.5","libVer":"1.0.0","author":"","repo":"","dep":[]}
 local dkjson = Require("dkjson")
 --- Identification number of the extension.
 --- Should be unique. Should be consistent in all references.
@@ -135,9 +135,8 @@ local function getListing(data)
     local page = data[PAGE]
     local document = GETDocument(expandURL("/browse/page/" .. page .. "/?sort=new&order=desc"))
     return map(document:select("#series-list-container > div > a"), function(v)
-        local img = v:selectFirst("div > div.aspect-2\\/3.relative.w-32.shrink-0 > img")
+        local img = v:selectFirst("img")
         img = img and img:attr("src") or imageURL
-        img = img or imageURL
         return Novel {
             title = v:selectFirst("h3"):text(),
             link = shrinkURL(v:attr("href")),
