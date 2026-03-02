0221c0765379e719d185860c6b47443a5d73c6c2
Levv616 <itsecretlev4@gmail.com>
2026-03-02 11:22:07 +0800

    [skip ci]

--- src/en/GravityTales.lua
+++ src/en/GravityTales.lua
@@ -1,7 +1,7 @@
--- {"id":1742321,"ver":"1.0.4","libVer":"1.0.0","author":"Lev616","dep":["Fictioneer>=2.2.0"]}
+-- {"id":981363135,"ver":"1.0.4","libVer":"1.0.0","author":"Lev616","dep":["Fictioneer>=2.2.0"]}
 
-return Require("Fictioneer")("https://gravitytales.com/", {
-    id = 1742321,
+return Require("Fictioneer")("https://gravitytales.com", {
+    id = 981363135,
     name = "Gravity Tales",
     imageURL = "https://lovelyblossoms.com/wp-content/uploads/2025/09/lovely-blossoms-2-Photoroom-1.png",
     listingSelector = "#list-of-stories li.card",
