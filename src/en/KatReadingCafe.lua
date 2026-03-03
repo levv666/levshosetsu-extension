e34c59bb4164ee5d77a529e46bb7a4af3dc758e0
Levv616 <itsecretlev4@gmail.com>
2026-03-03 20:22:24 +0800

    [skip ci]

--- src/en/KatReadingCafe.lua
+++ src/en/KatReadingCafe.lua
@@ -1,4 +1,4 @@
--- {"id":977513,"ver":"1.0.0","libVer":"1.0.0","author":"Lev616"}
+-- {"id":977513,"ver":"1.0.1","libVer":"1.0.0","author":"Lev616"}
 
 local baseURL = "https://katreadingcafe.com/"
 local CatReadingCafeLogo = "https://katreadingcafe.com/wp-content/uploads/2025/01/2717291-2-3-e1737942920628.png"
@@ -92,7 +92,11 @@ local function parseNovel(novelURL, loadChapters)
 
         local temp = map(chapterItems, function(v)
             local a = v:selectFirst("a")
-            local titleDiv = v:selectFirst(".epl-title")
+            local title = v:selectFirst(".epl-title")
+            local number = v:selectFirst(".epl-number")
+
+            -- You can then combine their text later
+            local titleDiv = title:text() .. " - " .. number:text()
 
             if a == nil or titleDiv == nil then
                 return nil
