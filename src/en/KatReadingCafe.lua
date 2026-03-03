94cc9364fad2aff86aed8a0c0f5cd39a24ed3c69
Levv616 <itsecretlev4@gmail.com>
2026-03-03 23:58:19 +0800

    [skip ci]

--- src/en/KatReadingCafe.lua
+++ src/en/KatReadingCafe.lua
@@ -1,4 +1,4 @@
--- {"id":977513,"ver":"1.1.6","libVer":"1.0.0","author":"Lev616"}
+-- {"id":977513,"ver":"1.1.9","libVer":"1.0.0","author":"Lev616"}
 
 local baseURL = "https://katreadingcafe.com/"
 local CatReadingCafeLogo = "https://katreadingcafe.com/wp-content/uploads/2025/01/2717291-2-3-e1737942920628.png"
@@ -80,12 +80,15 @@ local function createFilterString(data)
     end
 
     -- Order dropdown
-    local selectedOrderKey = data[301] -- dropdown key
-    if selectedOrderKey then
-        local orderValue = ORDER_OPTIONS[selectedOrderKey - 301 + 1].value
+    local selectedIndex = data[301]
+    if selectedIndex ~= nil then
+        -- Lua arrays start at 1, Shosetsu dropdown index starts at 0
+        local orderValue = ORDER_OPTIONS[selectedIndex + 1].value
         parts[#parts + 1] = "order=" .. orderValue
-    else parts[#parts + 1] = "order=update"
+    else
+        parts[#parts + 1] = "order=update"  -- default order
     end
+
 
 
     -- Page
