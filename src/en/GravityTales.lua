87942bf6c1cc0207f3e8bd67b8249492255e965c
Levv616 <itsecretlev4@gmail.com>
2026-03-03 12:50:38 +0800

    [skip ci]

--- src/en/GravityTales.lua
+++ src/en/GravityTales.lua
@@ -1,4 +1,4 @@
--- {"id":981363135,"ver":"1.0.4","libVer":"1.0.0","author":"Lev616","dep":["Fictioneer>=2.0.0"]}
+-- {"id":981363135,"ver":"1.0.5","libVer":"1.0.0","author":"Lev616","dep":["Fictioneer>=2.0.0"]}
 
 return Require("Fictioneer")("https://gravitytales.com", {
     id = 981363135,
@@ -6,7 +6,6 @@ return Require("Fictioneer")("https://gravitytales.com", {
     imageURL = "https://lovelyblossoms.com/wp-content/uploads/2025/09/lovely-blossoms-2-Photoroom-1.png",
     listingSelector = "#list-of-stories li.card",
     searchSelector = "#search-result-list li.card",
-    useSearchAsListing = true,
     chapterSelector = "li[data-group='unassigned']",
 
     listingPath = "/stories/page/",
