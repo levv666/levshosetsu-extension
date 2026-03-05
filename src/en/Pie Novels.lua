0fac7105c43353b77f5e86a0666fdbb32a0b77fb
Levv616 <itsecretlev4@gmail.com>
2026-03-06 00:22:44 +0800

    [skip ci]

--- src/en/Pie Novels.lua
+++ src/en/Pie Novels.lua
@@ -1,4 +1,4 @@
--- {"id":9915592,"ver":"1.0.2","libVer":"1.0.0","author":"Lev616"}
+-- {"id":9915592,"ver":"1.0.3","libVer":"1.0.0","author":"Lev616"}
 
 local baseURL = "https://pienovels.com"
 local PieNovelsLogo = "https://pienovels.com/wp-content/uploads/2025/01/logo-pie-png.webp"
@@ -29,7 +29,7 @@ local function parseListing(listingURL)
 
         local imgEl = card:selectFirst("img")
 
-        print("title : " + titleEl:text())
+        print("title : " + title)
         print("link: " + shrinkURL(linkEl:attr("href") or ""))
         print("img : " + imgEl and imgEl:attr("src"))
 
