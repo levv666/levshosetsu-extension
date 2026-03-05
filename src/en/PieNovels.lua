9033ccf5b3512367128f87b728db163848bb14c1
Levv616 <itsecretlev4@gmail.com>
2026-03-06 02:03:28 +0800

    [skip ci]

--- src/en/PieNovels.lua
+++ src/en/PieNovels.lua
@@ -1,4 +1,4 @@
--- {"id":9915592,"ver":"1.1.1","libVer":"1.0.0","author":"Lev616"}
+-- {"id":9915592,"ver":"1.1.2","libVer":"1.0.0","author":"Lev616"}
 
 local baseURL = "https://pienovels.com"
 local PieNovelsLogo = "https://pienovels.com/wp-content/uploads/2025/01/logo-pie-png.webp"
@@ -51,7 +51,7 @@ end
 local function search(data)
     local query = data[QUERY] or ""
     local page = data[PAGE]
-    local url = baseURL .. "/novels/?search=" .. query .. "page=" .. page .. "&sort_by=latest_updated"
+    local url = baseURL .. "/novels/?search=" .. query .. "&page=" .. page .. "&sort_by=latest_updated"
     return parseListing(url)
 end
 
