f995d7c2d2b6531ab4f578794036c399602994fd
Levv616 <itsecretlev4@gmail.com>
2026-03-06 01:26:33 +0800

    [skip ci]

--- src/en/Pie Novels.lua
+++ src/en/Pie Novels.lua
@@ -1,4 +1,4 @@
--- {"id":9915592,"ver":"1.0.3","libVer":"1.0.0","author":"Lev616"}
+-- {"id":9915592,"ver":"1.0.4","libVer":"1.0.0","author":"Lev616"}
 
 local baseURL = "https://pienovels.com"
 local PieNovelsLogo = "https://pienovels.com/wp-content/uploads/2025/01/logo-pie-png.webp"
@@ -118,7 +118,7 @@ local function parseNovel(novelURL, loadChapters)
 
         -- Sort by data-id ascending
         table.sort(temp, function(a, b)
-            return a.id < b.id
+            return a.title < b.title
         end)
 
         -- Convert to Shosetsu List
