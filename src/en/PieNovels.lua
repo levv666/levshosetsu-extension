1389b0eae61d414241fe21602921edd25b2ec43d
Levv616 <itsecretlev4@gmail.com>
2026-03-06 02:01:48 +0800

    [skip ci]

--- src/en/PieNovels.lua
+++ src/en/PieNovels.lua
@@ -1,4 +1,4 @@
--- {"id":9915592,"ver":"1.1.0","libVer":"1.0.0","author":"Lev616"}
+-- {"id":9915592,"ver":"1.1.1","libVer":"1.0.0","author":"Lev616"}
 
 local baseURL = "https://pienovels.com"
 local PieNovelsLogo = "https://pienovels.com/wp-content/uploads/2025/01/logo-pie-png.webp"
@@ -50,37 +50,44 @@ end
 
 local function search(data)
     local query = data[QUERY] or ""
-    local page  = data[PAGE] or 1
-    local url = baseURL .. "/novels/?search=" .. query .. "&page=" .. page
-
-    local doc = GETDocument(url)
-    if not doc then return {} end
-
-    return mapNotNil(doc:select("div.novel-grid div.novel-item"), function(card)
-        local linkEl  = card:selectFirst("a")
-        local titleEl = card:selectFirst("div.novel-content-text h1")
-        if not (linkEl and titleEl) then return nil end
-
-        local href = linkEl:attr("href") or ""
-        local slug = href:match("/novels/([^/]+)/?")
-        local title = slug and slug:gsub("-", " ") or titleEl:text()
-        title = title:gsub("(%S)(%S*)", function(first, rest)
-            return first:upper() .. rest:lower()
-        end)
-
-        local imgEl = card:selectFirst("img")
-
-        print("tittle : " .. title)
-        print("link : " .. shrinkURL(linkEl:attr("href") or ""))
-        print("image : " .. (imgEl and imgEl:attr("src")))
-
-        return Novel {
-            title = title,
-            link = shrinkURL(linkEl:attr("href") or ""),
-            imageURL = imgEl and imgEl:attr("src")
-        }
-    end)
+    local page = data[PAGE]
+    local url = baseURL .. "/novels/?search=" .. query .. "page=" .. page .. "&sort_by=latest_updated"
+    return parseListing(url)
 end
+
+--local function search(data)
+--    local query = data[QUERY] or ""
+--    local page  = data[PAGE] or 1
+--    local url = baseURL .. "/novels/?search=" .. query .. "&page=" .. page
+--
+--    local doc = GETDocument(url)
+--    if not doc then return {} end
+--
+--    return mapNotNil(doc:select("div.novel-grid div.novel-item"), function(card)
+--        local linkEl  = card:selectFirst("a")
+--        local titleEl = card:selectFirst("div.novel-content-text h1")
+--        if not (linkEl and titleEl) then return nil end
+--
+--        local href = linkEl:attr("href") or ""
+--        local slug = href:match("/novels/([^/]+)/?")
+--        local title = slug and slug:gsub("-", " ") or titleEl:text()
+--        title = title:gsub("(%S)(%S*)", function(first, rest)
+--            return first:upper() .. rest:lower()
+--        end)
+--
+--        local imgEl = card:selectFirst("img")
+--
+--        print("tittle : " .. title)
+--        print("link : " .. shrinkURL(linkEl:attr("href") or ""))
+--        print("image : " .. (imgEl and imgEl:attr("src")))
+--
+--        return Novel {
+--            title = title,
+--            link = shrinkURL(linkEl:attr("href") or ""),
+--            imageURL = imgEl and imgEl:attr("src")
+--        }
+--    end)
+--end
 
 
 -- =========================
