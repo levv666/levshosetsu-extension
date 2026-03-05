d9512eef5af5f6e7ea88abf2faa8bdf32a9f30fe
Levv616 <itsecretlev4@gmail.com>
2026-03-06 00:21:27 +0800

    [skip ci]

--- src/en/Pie Novels.lua
+++ src/en/Pie Novels.lua
@@ -1,4 +1,4 @@
--- {"id":9915592,"ver":"1.0.1","libVer":"1.0.0","author":"Lev616"}
+-- {"id":9915592,"ver":"1.0.2","libVer":"1.0.0","author":"Lev616"}
 
 local baseURL = "https://pienovels.com"
 local PieNovelsLogo = "https://pienovels.com/wp-content/uploads/2025/01/logo-pie-png.webp"
